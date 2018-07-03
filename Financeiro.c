29ece7b8b471059cc168ffa990452ac15302fd5e
pedrolopesjc <40678796+pedrolopesjc@users.noreply.github.com>
2018-07-03 17:26:26 -0300

    Update Financeiro.c

--- Financeiro.c
+++ Financeiro.c
@@ -9,10 +9,10 @@ typedef struct cadProduto {
     float precoVenda;
 } Produto;
 
-FILE *file;
-
 main(){
     
+    FILE *file, *file2;
+        
     file = fopen("produtos.txt", "r"); //Verificar nome do arquivo
     
     if (file==NULL){
@@ -22,10 +22,8 @@ main(){
     
     Produto produto[10];
     int i;
-   
-    
+
     for (i=0; i<10; i++){
-    
         fscanf(file, "%d\n", &produto[i].codigo);
         fgets(produto[i].descricao, 100, file);
         fscanf(file, "%d\n", &produto[i].qtd);
@@ -63,14 +61,33 @@ main(){
         }    
     }while(controle==1);
     
+    controle =0;
+    
     printf ("Quantidade de produtos vendidos: ");
     scanf ("%d", &produtoEncotrado.qtd);
+    printf ("\n\n");
+    
+    printf ("Código:         %d\n", produtoEncotrado.codigo);
+    printf ("%s\n", produtoEncotrado.descricao);
+    printf ("Quantidade:     %d", produtoEncotrado.qtd);
+    printf ("Preço de venda: %f\n\n", produtoEncotrado.precoVenda);
+    
+    printf ("Digite 1, se deseja lançar venda, 0 caso não: ");
+    scanf ("%d", &controle);
+    
+    if(controle!=1) exit(0);
+    
     
     valorVenda = produtoEncotrado.precoVenda*produtoEncotrado.qtd;
     valorLucro = valorVenda - produtoEncotrado.precoCusto*produtoEncotrado.qtd;
     
-    totalLucro = totalLucro + valorLucro;
+    file2 = fopen ("financeiro.txt", "a");
     
-    printf("Total Custo %f, Total Venda %f, Total Lucro %f", totalCusto, totalVenda, totalLucro);
+    fscanf(file, "%f\n", &totalVenda);
+    fscanf(file, "%f\n", &totalLucro);
     
+    fprintf ("%f\n", codigo);
+    fprintf ("%d\n", )
+        
+    fclose(file2);
 }
