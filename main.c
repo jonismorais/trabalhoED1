c05fcb224c77c59b776dbf61c78e4d37d4f54647
pedrolopesjc <40678796+pedrolopesjc@users.noreply.github.com>
2018-07-10 11:55:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
-#include <conio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <locale.h>
+#include <time.h>
+#include <stdbool.h>
+
+//time_t converte time para string
+typedef struct relatorio {
+  char dia;
+  char mes;
+  char codigo;
+  float valorCust;
+  float valorLucr;
+  struct Relatorio *prox;
+
+}Relatorio;
+
+struct cadProduto {
+    char codigo[9];
+    char descricao[100];
+    int qtd, qtdVenda;
+    float precoCusto;
+    float precoVenda;
+} Produto;
 
 struct cadCliente {
     char codigo[9];
@@ -21,13 +41,7 @@ struct cadFornecedor {
     char status;
 } Fornecedor;
 
-struct cadProduto {
-    char codigo[9];
-    char descricao[100];
-    int qtd, qtdVenda;
-    float precoCusto;
-    float precoVenda;
-} Produto;
+
 
 FILE *fp;
 /*Funções do Sistema*/
@@ -41,8 +55,15 @@ void ajuda(void);
 int financeiro(void);
 void subMenu(void);
 void subMenuCadastros(void);
+void relat(void);
 
 int iTag = 0;
+
+Relatorio *novo;
+Relatorio *inicio = NULL;
+Relatorio *fim = NULL;
+Relatorio *aux = NULL;
+Relatorio *anterior = NULL;
 
 int main(void) {
     setlocale(LC_ALL, "Portuguese");
@@ -72,6 +93,8 @@ int main(void) {
             case 'c': subMenu();
                 break;
             case 'f': financeiro();
+                break;
+            case 'r': relat();
                 break;
             case 'h': ajuda();
                 break;
@@ -695,10 +718,15 @@ int financeiro(void) {
     int pos, contProd;
     float totalVenda, valorVenda, valorLucro, tLucroProd;
 
+    time_t data;
+    time(&data);
+    struct tm *d = localtime(&data);
+
     tLucroProd=0;
     iTag=3;
     controle[3]='nao';
     totalVenda=0;
+
 
     do {
         pos=busca();
@@ -749,27 +777,178 @@ int financeiro(void) {
             fscanf(fp, "%d\n", &Produto.qtdVenda);
             fscanf(fp, "%f\n", &Produto.precoCusto);
     }else{
-        contProd =0;
+        contProd = 0;
     }
 
     fclose(fp);
-
-        printf("%.2f", totalVenda);
         totalVenda = totalVenda + valorVenda;
+        printf("%.2f", totalVenda);
+
 
 
     fopen("financeiro.txt", "a");
 
-        fprintf(fp, "%s\n", Produto.codigo);
-        fprintf(fp, "%s\n", Produto.descricao);
-        fprintf(fp, "%d\n", Produto.qtd);
-        fprintf(fp, "%d\n", Produto.qtdVenda);
-        fprintf(fp, "%.2f\n", Produto.precoCusto);
+    fprintf(fp, "%s\n", Produto.codigo);
+    fprintf(fp, "%s\n", Produto.descricao);
+    fprintf(fp, "%d\n", Produto.qtd);
+    fprintf(fp, "%d\n", Produto.qtdVenda);
+    fprintf(fp, "%.2f\n", Produto.precoCusto);
+
+    fclose(fp);
+    novo=(Relatorio*)malloc(sizeof(Relatorio));
+    novo->dia = (d->tm_mday);
+    novo->mes = (d ->tm_mon + 1);
+    novo->valorCust = Produto.precoCusto;
+    novo->valorLucr = valorLucro;
+
+
+    fopen("relatorio.txt", "a");
+
+    fprintf(fp, "%d\n", novo->dia);
+    fprintf(fp, "%d\n", novo->mes);
+    fprintf(fp, "%s\n", Produto.codigo);
+    fprintf(fp, "%.2f\n", novo->valorCust);
+    fprintf(fp, "%.2f\n", novo->valorLucr);
 
     fclose(fp);
 
-    printf("Gravado com suscesso!\n\n");
+    novo->prox = NULL;
+    if(inicio == NULL){
+      inicio = novo;
+      fim = novo;
+    }else{
+      fim->prox = novo;
+      fim=novo;
+    }
+
+    printf("\n\n\tGravado com suscesso!\n\n");
     system("pause");
 
     main();
+}
+
+
+int buscarRelatorio(int dia, int mes, int cont){
+
+  system("cls");
+
+  int diaInicio, diaFim, mesInicio, mesFim;
+  char controle[3];
+
+
+  if ((fp = fopen("relatorio.txt", "rd")) == NULL) {
+      printf("\n O arquivo nao pode ser aberto!!\n");
+      getch();
+      exit(1);
+  }
+
+  printf("\nDigite o dia que deseja o relatorio:");
+  gets(dia);
+  printf("\nDigite o mes que deseja o relatorio:");
+  gets(mes);
+  setbuf(stdin, NULL);
+  rewind(fp);
+
+  if(inicio==NULL){
+    printf("\n\n\tARQUIVO VAZIO\n\n\tTente novamente...");
+    system("pause");
+    main();
+  } else {
+    printf("\nPercorrendo todo o relatorio...\n");
+    aux = inicio;
+    while(aux != NULL){
+      printf("%d", aux->dia);
+      aux=aux->prox;
+    }
+  }
+
+  fclose(fp);
+}
+
+void relat(){
+    time_t data;
+    time(&data);
+    struct tm *d = localtime(&data);
+    int diaInicio, diaFim, mesInicio, mesFim;
+    char menu[2], men, controle[3];
+    int contt=0;
+    double somaLucro = 0, somaCusto = 0;
+
+  do{
+    do{
+      system("cls");
+      printf("\n\n");
+      printf("\t########################################################\n");
+      printf("\t#                                                      #\n");
+      printf("\t#         Programa de Relatórios Financeiros          #\n");
+      printf("\t#                                                      #\n");
+      printf("\t########################################################\n\n\n");
+      printf("\n Digite uma das opções que deseja obter o relatório\n\n");
+      printf("\n <P> - Por período");
+      printf("\n <D> - Relatório do dia");
+      printf("\n <V> - Voltar");
+      printf("\n\n\n Digite a Sua Opção:");
+      gets(menu);
+      setbuf(stdin, NULL);
+      men = tolower(*menu);
+    }while(!strchr("cfpv", men));
+    switch (men) {
+        case 'p':
+            printf("Digite o dia de inicio..");
+            gets(diaInicio);
+            printf("Digite o mes de inicio..");
+            gets(mesInicio);
+            printf("Digite o dia de fim..");
+            gets(diaFim);
+            printf("Digite o mes de fim..");
+            gets(mesFim);
+            setbuf(stdin,NULL);
+            if(mesFim==mesInicio){
+              contt = diaFim - diaInicio;
+              buscarRelatorio(diaInicio, mesInicio, contt);
+            } else if(mesFim!=mesInicio) {
+
+            }
+            system("cls");
+            printf("\nDeseja continuar no programa? <sim/nao>:  ");
+            gets(controle);
+            setbuf(stdin,NULL);
+            if((strcmp(controle, "sim") != 0)&&(strcmp(controle, "SIM") != 0)){
+              main();
+            }else{
+              exit(0);
+            }
+            break;
+        case 'd':
+          novo->dia = (d->tm_mday);
+          novo->mes = (d ->tm_mon + 1);
+          if(inicio==NULL){
+            printf("\n\n\tARQUIVO VAZIO\n\n\tTente novamente...");
+            system("pause");
+            main();
+          } else {
+            printf("\nPercorrendo todo o relatorio...\n");
+            aux = inicio;
+            printf("\n\n\tRELATORIO DO DIA %d / %d\n\n", (d->tm_mday), (d ->tm_mon + 1));
+            while(aux != NULL){
+              if((aux==diaInicio)&&(aux->prox==mesInicio)){
+                somaLucro += aux->valorLucr;
+                somaCusto += aux->valorCust;
+              }
+              aux=aux->prox;
+            }
+            fclose(fp);
+            printf("\n\tLucro do Dia: %.2d", somaLucro);
+            printf("\n\tValor Custo: %.2d\n\n", somaCusto);
+            printf("\n\n\nCaro usuario, não se esqueça de investir seu lucro, tenha consciencia que ao menos 5%% deve ser guardado em poupança..\n");
+            }
+            relat();
+            break;
+        case 'v':
+            main();
+            break;
+        default: relat();
+    }
+  }while(1);
+
 }
